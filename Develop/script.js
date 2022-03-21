4238ab756727ce7b4aca7aef37318f47449af320
Emma Olson <emma.olson794@gmail.com>
2022-03-21 11:27:31 -0500

    define generate password function

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,36 @@
-// Assignment code here
+//onclick (button) user is presented with password criteria prompts
+//select criteria to include in password
+  //choose a length 8-128 characters
+  //choose character types (lowercase, uppercase, numeric, special characters)
+  //validate input (at least one character type must be selected)
+//when all prompts are answered, then generate a password matching criteria
+//when generated, password is displayed in alert or written to the page
 
+// password criteria array
+var passwordCriteria = ["LOWERCASE", "UPPERCASE", "NUMERIC", "SPECIAL"];
+
+// define generatePassword function
+var generatePassword = function(){
+  window.alert("Please follow the prompts to select your password criteria.");
+
+// ask user to input length of password
+  let passwordLength = window.prompt("Please choose your password length (8-128 characters)");
+  console.log(passwordLength);
+// store that information for later
+
+// for loop cycling through array 
+  for (var i = 0; i < passwordCriteria.length; i++) {
+    var pickedCriteria = passwordCriteria[i]
+    var promptCriteria = window.prompt("Would you like to include " + pickedCriteria + " characters? Enter 'YES' or 'NO'");
+
+    // if yes, store information for later
+    if (promptCriteria === "yes" || promptCriteria === "YES") {
+      console.log(pickedCriteria);
+    }
+    // if no, nothing happens
+    else {}
+  }
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +46,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
