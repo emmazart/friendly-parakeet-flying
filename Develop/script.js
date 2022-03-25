f6f85c00f290b4b84b65cf5c1878dfe0eea6c4be
Emma Olson <emma.olson794@gmail.com>
2022-03-25 12:29:20 -0500

    randomize character selection

--- Develop/script.js
+++ Develop/script.js
@@ -9,6 +9,8 @@
 // password criteria array
 var passwordCriteria = ["LOWERCASE", "UPPERCASE", "NUMERIC", "SPECIAL"];
 var userPickedCriteria = [];
+var passwordArray = [];
+
 
 // define function to read userPickedCriteria
 var passwordContent = [];
@@ -20,29 +22,50 @@ var readPickedCriteria = function() {
   } else {
     console.log("MATCHED! UPPERCASE")
     // define a variable associated with that criteria
-    let upper = ["apples"];
+    var upper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+    // and add it to the password possibilities
     passwordContent = (passwordContent + upper);
-    // and add it to the password
+    // and add 2 guaranteed characters to the passwordArray
+    for (let i = 0; i < 2; i++) {
+      var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+      var randomCharacter = getRandom(upperArray);
+      passwordArray.push(randomCharacter);
+    }  
   }
   if (!toMatch.match("LOWERCASE")){
   } else {
     console.log("MATCHED! LOWERCASE")
-    let lower = ["oranges"];
+    let lower = ["abcdefghijklmnopqrstuvwxyz"];
     passwordContent = (passwordContent + lower);
+    for (let i = 0; i < 2; i++) {
+      var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+      var randomCharacter = getRandom(lowerArray);
+      passwordArray.push(randomCharacter);
+    }  
   }
   if (!toMatch.match("NUMERIC")){
   } else {
     console.log("MATCHED! NUMERIC")
-    let num = ["bananas"];
+    let num = ["0123456789"];
     passwordContent = (passwordContent + num);
+    for (let i = 0; i < 2; i++) {
+      var numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+      var randomCharacter = getRandom(numArray);
+      passwordArray.push(randomCharacter);
+    }
   }
   if (!toMatch.match("SPECIAL")) {
   } else {
     console.log("MATCHED! SPECIAL")
-    let spec = ["grapes"];
+    let spec = ["!@#$%^&*"];
     passwordContent = (passwordContent + spec);
+    for (let i = 0; i < 2; i++) {
+      var specArray = ["!", "@", "#", "$", "%", "^", "&", "*"];
+      var randomCharacter = getRandom(specArray);
+      passwordArray.push(randomCharacter);
   }
-  console.log(passwordContent);
+  passwordContent = String(passwordContent);
+};
 };
 
 // define generateCriteria function
@@ -77,7 +100,7 @@ var generatePassword = function(){
   window.alert("Please follow the prompts to select your password criteria.");
 
   // ask user to input length of password
-  let passwordLength = window.prompt("Please choose your password length (8-128 characters)");
+  var passwordLength = window.prompt("Please choose your password length (8-128 characters)");
   // validate password length input
   if (passwordLength < 8 || passwordLength > 128){
     window.alert("You must enter a valid password length.")
@@ -85,6 +108,7 @@ var generatePassword = function(){
   }
   else {
     console.log(passwordLength);
+    passwordLength = parseInt(passwordLength);
   }
 
   // define generateCriteria function to prompt through criteria array & receive user input
@@ -97,13 +121,31 @@ var generatePassword = function(){
   }
   else {
     console.log(userPickedCriteria);
-    // return userPickedCriteria;
   };
 
   // IF ARRAY contains "CRITERIA" then do this?
   readPickedCriteria();
-  return passwordContent;
+  // display passwordContent to page
+  console.log(passwordContent);
+
+  // turn passwordContent into an array
+  passwordContent = passwordContent.split("")
+  // subtract guaranteed characters from passwordLength input
+  passwordLength = (passwordLength - passwordArray.length);
+  // loop through passwordContent as many times as our password character length
+  for (let i = 0; i < passwordLength; i++) {
+    var randomCharacter = getRandom(passwordContent);
+    passwordArray.push(randomCharacter);
+  }
+  console.log(passwordArray);
+  return passwordArray.join("");
 };
+
+function getRandom(arr){
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  var randomElement = arr[randomIndex];
+  return randomElement
+}
 
 
 // Get references to the #generate element
